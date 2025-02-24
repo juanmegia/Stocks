82c8195a699e978dccc3c5f9edfc1eb1c7521b49
juanmegia <juanmegia01@gmail.com>
2025-02-24 21:20:33 +0100

    serialization for complex types

--- app/src/main/java/com/example/architectcoders/data/StockDetail.kt
+++ app/src/main/java/com/example/architectcoders/data/StockDetail.kt
@@ -2,6 +2,8 @@ package com.example.architectcoders.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.architectcoders.data.datasource.remote.Converters
 
 @Entity
 data class StockDetail(
@@ -14,6 +16,7 @@ data class StockDetail(
     val phone: String,
     val website: String,
     val fullTimeEmployees: Int,
+    @TypeConverters(Converters::class)
     val companyOfficers: List<CompanyOfficerSummary>,
     val isFavorite: Boolean = false
 )
--- app/src/main/java/com/example/architectcoders/data/datasource/database/SymbolsDatabase.kt
+++ app/src/main/java/com/example/architectcoders/data/datasource/database/SymbolsDatabase.kt
@@ -2,10 +2,13 @@ package com.example.architectcoders.data.datasource.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.architectcoders.data.Stock
 import com.example.architectcoders.data.StockDetail
+import com.example.architectcoders.data.datasource.remote.Converters
 
 @Database(entities = [Stock::class, StockDetail::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)  // Registrar el type converter
 abstract class SymbolsDatabase: RoomDatabase() {
     abstract fun symbolsDao(): SymbolsDao
 }
